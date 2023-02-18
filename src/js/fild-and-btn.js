462fb74d33c6f477503079c354ca054a71524be7
Andrii-Pershko <andriiinf@gmail.com>
2023-02-18 22:45:20 +0200

    ok

---
+++ src/js/fild-and-btn.js
@@ -0,0 +1,101 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.search'),
+  input: document.querySelector('.searchTerm'),
+  API_KEY: '33728720-baaaf621421e045403ddcb3ff',
+  galery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
+  URL: 'https://pixabay.com/api/',
+  page: 1,
+
+}
+
+refs.form.addEventListener('submit', onCliclBtn);
+refs.loadMore.addEventListener('click', loadMoreImg);
+
+
+function onCliclBtn(e) {
+  refs.loadMore.style.display = 'none';
+  e.preventDefault();
+  refs.galery.innerHTML = '';
+  refs.page = 1;
+
+  getData()
+
+
+}
+
+function loadMoreImg() {
+  refs.page += 1;
+  getData();
+
+}
+
+function getData() {
+  const { input, API_KEY, URL, page } = refs;
+
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: input.value.trim().replaceAll(' ', '+'),
+    per_page: 40,
+    page: page,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+
+  axios.get(`${URL}?${params}`).then(response => {
+    refs.loadMore.style.display = 'block';
+    if (response.data.total === 0) {
+      Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+      return
+    }
+    if (response.data.hits.length < 40) {
+      Notify.failure("We're sorry, but you've reached the end of search results.")
+      refs.loadMore.style.display = 'none';
+    }
+    if (refs.input.value === '') {
+      Notify.failure("You have not entered anything ");
+      return
+    }
+    console.log(response.data.hits)
+    markUpImg(response.data.hits);
+
+  }
+  )
+}
+
+
+function markUpImg(arryImg) {
+  const markUp = arryImg.map((el) => {
+    const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = el;
+    return `
+        <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes </b>
+            <span>${likes}</span>
+
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            <span>${views}</span>
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            <span>${comments}</span>
+          </p>
+          <p class="info-item">
+            <b>Downloads </b>
+            <span>${downloads}</span>
+            
+          </p>
+        </div>
+      </div>`
+  }).join(" ");
+  refs.galery.insertAdjacentHTML("beforeend", markUp);
+}
